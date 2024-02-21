ccef615eaf7a1150a5fa4b1742568129d3217d35
Baha Fayad <baha.fayad@elev.ga.ntig.se>
2024-02-21 16:32:32 +0100

    Laddat ner assets och gjorde en basic movement for player

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update¨
+
+    [SerializeField] float speed = 5;
+    [SerializeField] float jumpForce =600;
+
+    private Animator animator;
+    private Rigidbody2D rb;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"),0) *Time.deltaTime *speed;
+        Vector2 jumpMovement = new Vector2(0,Input.GetAxisRaw("Vertical"));
+
+        string LookingRight = "LookingRight";
+        if(movement.x > 0)
+        {
+            animator.SetBool(LookingRight,true);
+        }
+        else if(movement.x < 0)
+        {
+
+            animator.SetBool(LookingRight,false);
+        }
+
+
+        rb.AddForce(jumpMovement *jumpForce);
+        transform.Translate(movement);
+    }
+}
